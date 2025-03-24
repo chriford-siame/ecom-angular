fb10c039debeee6ad93531c11947a4039df6e256
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 16:06:23 +0200

    enable dynamic cart item removal

--- src/app/pages/cart/cart-item/cart-item.component.ts
+++ src/app/pages/cart/cart-item/cart-item.component.ts
@@ -1,9 +1,11 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Product } from '../../../models/products.model';
+import { ButtonComponent } from "../button/button.component";
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-cart-item',
-  imports: [],
+  imports: [ButtonComponent],
   template: `
     <div class="bg-white shadow-md rounded-xl p-6 flex gap-4 items-center">
       <img [src]="item().image" class="w-[50px] h-[50px] object-contain" alt="">
@@ -11,10 +13,14 @@ import { Product } from '../../../models/products.model';
         <span class="text-md font-bold">{{item().title}}</span>
         <span class="text-sm">{{'$' + item().price}}</span>
       </div>
+      <div class="flex-1"></div>
+      <app-button label="Remove" (action)="cartService.removeItem(item().id)" />
     </div>
   `,
   styles: ``
 })
 export class CartItemComponent {
   item = input.required<Product>();
+
+  cartService = inject(CartService);
 }
