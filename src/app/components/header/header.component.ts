c827db8dfd8d362b64ad53bc39e0204eced4163a
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 11:17:11 +0200

    Add content

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <p>
-      header works!
-    </p>
-  `,
+    <div class="bg-slate-100 px-4 py-5 shadow-md flex justify-between items-center">
+      <span>My Store</span>
+      <div>
+        <app-primary-button label="Cart" (action)="cartItemViewer()" />
+      </div>
+    </div>
+    `,
   styles: ``
 })
 export class HeaderComponent {
+  cartItemViewer() {
+    console.log("my cart!")
+  }
 
 }
