f30b2e3eadc3840dc1ea2d8af5f1aa8b539db76f
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 16:05:57 +0200

    Add cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,15 +1,20 @@
 import { Injectable, signal } from '@angular/core';
 import { Product } from '../models/products.model';
+import { CART } from '../fixture/cart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  cartItems = signal<Product[]>([]);
+  cartItems = signal<Product[]>(CART);
 
   addToCart(product: Product) {
     this.cartItems.set([...this.cartItems(), product])
+  }
+
+  removeItem(id: number) {
+    this.cartItems.set(this.cartItems().filter((p) => p.id !== id));
   }
 
   constructor() { }
