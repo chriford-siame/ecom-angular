f52a67837e76a4a6531b2d939a87cc161d387c9f
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 13:29:29 +0200

    Update cart component

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Product } from '../../models/products.model';
+import { CartItemComponent } from "./cart-item/cart-item.component";
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CartItemComponent],
   template: `
-    <p>
-      cart works!
-    </p>
+    <div class="p-8 flex flex-col gap-4">
+      <p class="h2">Shopping Cart</p>
+      @for (item of cartItems(); track item.id) {
+        <app-cart-item [product]="item" />
+      }
+    </div>
   `,
   styles: ``
 })
 export class CartComponent {
-
+  cartItems = signal<Product[]>([]);
 }
