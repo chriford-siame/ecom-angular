ba0c255deb6daef2b9e1348551e4c12f777b8046
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 12:17:23 +0200

    Add stock indicator

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Product } from '../../models/products.model';
+import { PRODUCTS } from '../../fixture/products';
+import { ProductCardComponent } from "./product-card/product-card.component";
 
 @Component({
   selector: 'app-products-list',
-  imports: [],
+  imports: [ProductCardComponent],
   template: `
-    <p>
-      products-list works!
-    </p>
+    <div class="p-8 grid grid-cols-2 gap-4">
+      @for (product of products(); track product.id) {
+        <app-product-card [product]="product" />
+      }
+    </div>
   `,
   styles: ``
 })
 export class ProductsListComponent {
-
+  products = signal<Product[]>(PRODUCTS)
 }
