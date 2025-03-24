02c55e8264b4c7f330392ddb644347ecb64815a1
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 16:23:21 +0200

    implement component

---
+++ src/app/pages/cart/order-summary/order-summary.component.ts
@@ -0,0 +1,31 @@
+import { Component, computed, inject } from '@angular/core';
+import { CartService } from '../../../services/cart.service';
+import { PrimaryButtonComponent } from "../../../components/primary-button/primary-button.component";
+
+@Component({
+  selector: 'app-order-summary',
+  imports: [PrimaryButtonComponent],
+  template: `
+    <div class="bg-slate-100 p-6 rounded-xl shadow-xl">
+      <h2 class="text-2xl">Order Summary</h2>
+      <div class="flex flex-col gap-4">
+        <div class="flex mt-2 gap-4">
+          <span class="text-lg">Total</span>
+          <span class="text-lg font-bold">{{'$' + total()}}</span>
+        </div>
+        <app-primary-button label="Proceed to checkout" />
+      </div>
+    </div>
+  `,
+  styles: ``
+})
+export class OrderSummaryComponent {
+  cartService = inject(CartService);
+  total = computed(() => {
+    let total = 0;
+    for(const item of this.cartService.cartItems()) {
+      total += item.price;
+    }
+    return total;
+  })
+}
