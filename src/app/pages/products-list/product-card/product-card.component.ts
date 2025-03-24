561c467b820eaf29ffa5814c9957e3058a3f96af
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 12:04:25 +0200

    Update a class attr

--- src/app/pages/products-list/product-card/product-card.component.ts
+++ src/app/pages/products-list/product-card/product-card.component.ts
@@ -1,12 +1,20 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../../../models/products.model';
+import { PrimaryButtonComponent } from "../../../components/primary-button/primary-button.component";
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <div>
-      {{product().id}}
+    <div class="bg-white shadow-md rounded-xl p-6 flex flex-col relative">
+      <div class="mx-auto">
+        <img [src]="product().image" class="h-[100px] w-[200px] object-contain" alt="image">
+      </div>
+      <div class="flex flex-col">
+        <span class="text-md font-bold">{{product().title}}</span>
+        <span class="text-md font-bold">{{product().price}}</span>
+        <app-primary-button label="Add to Cart" class="mt-3 w-full"/>
+      </div>
     </div>
   `,
   styles: ``
