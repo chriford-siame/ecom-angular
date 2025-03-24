730a991278387ca02f9a6360e18dc6ccacd65996
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 11:06:50 +0200

    Add button signals

--- src/app/components/primary-button/primary-button.component.ts
+++ src/app/components/primary-button/primary-button.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
   imports: [],
   template: `
-    <p>
-      primary-button works!
-    </p>
+    <button (click)="handleButtonClick()">
+      {{ label() }}
+    </button>
   `,
   styles: ``
 })
 export class PrimaryButtonComponent {
+  label = input('');
 
+  btnClicked = output();
+
+  handleButtonClick() {
+    this.btnClicked.emit();
+    console.log("Button clicked!!")
+  }
 }
