d8983ed2dc23474103eeae65af0f50d43fe4c106
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 12:54:18 +0200

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { ProductCardComponent } from './pages/products-list/product-card/product-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ProductsListComponent,
+  },
+  {
+    path: 'cart',
+    component: ProductCardComponent,
+  },
+];
