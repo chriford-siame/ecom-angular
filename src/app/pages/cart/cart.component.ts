e246d54a4dd83971f8e91e3ba7c9f238a396eeb3
chriford-siame <chrifordsiame@gmail.com>
2025-03-24 16:23:34 +0200

    utilize order summary component

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { CartItemComponent } from "./cart-item/cart-item.component";
 import { CartService } from '../../services/cart.service';
+import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItemComponent],
+  imports: [CartItemComponent, OrderSummaryComponent],
   template: `
     <div class="p-8 flex flex-col gap-4">
       <p class="text-2xl font-bold">Shopping Cart</p>
       @for (item of cartService.cartItems(); track item.id) {
         <app-cart-item [item]="item" />
       }
+      <app-order-summary />
     </div>
   `,
   styles: ``
